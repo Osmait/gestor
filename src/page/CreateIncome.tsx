5af47b24aa9617142f10b0400d1d032232e948df
Saul Burgos <saulburgos6@gmail.com>
2022-11-27 20:10:38 -0400

    add form income and bill

--- src/page/CreateIncome.tsx
+++ src/page/CreateIncome.tsx
@@ -3,14 +3,18 @@ import { Link } from "react-router-dom";
 
 export const CreateIncome = () => {
   return (
-    <div className="login">
-      <button>
-        <Link className="link" to={"/"}>
-          Index
-        </Link>
-      </button>
-      <h1>Registrar Ingreso</h1>
-      <FromIncome />
+    <div className="min-h-screen bg-neutral-900  grid  grid-rows-3  max-h-screen">
+      <div>
+        <h2 className="text-white text-8xl">Ingresos</h2>
+        <a className=" text-white text-2xl">
+          <Link className="link" to={"/"}>
+            Inicio
+          </Link>
+        </a>
+      </div>
+      <div className="bg-neutral-800 md:w-2/8 lg:w-2/8 mx-auto shadow rounded-lg p-15 row-start-2">
+        <FromIncome />
+      </div>
     </div>
   );
 };
